8d7cada48f4ef0394673e5055cea7dc0199c97dd
estratus <johantapiaosorio@gmail.com>
2025-04-28 06:30:40 -0500

    main

---
+++ data.js
@@ -0,0 +1,193 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-porshe",
+      "name": "porshe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.17988028957709,
+          "pitch": 0.24666550818407273,
+          "rotation": 0,
+          "target": "0-porshe"
+        },
+        {
+          "yaw": 2.5341134550663096,
+          "pitch": 0.11012741625674138,
+          "rotation": 0,
+          "target": "3-bao"
+        },
+        {
+          "yaw": 3.0557105577018167,
+          "pitch": 0.12277281508508509,
+          "rotation": 0,
+          "target": "2-sala-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dormitorio",
+      "name": "dormitorio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6652329855218273,
+          "pitch": 0.10317941316106172,
+          "rotation": 0,
+          "target": "3-bao"
+        },
+        {
+          "yaw": 1.2835846848347892,
+          "pitch": 0.05404193817258118,
+          "rotation": 0,
+          "target": "0-porshe"
+        },
+        {
+          "yaw": 2.689317095380554,
+          "pitch": 0.023566633814601445,
+          "rotation": 0,
+          "target": "2-sala-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sala-principal",
+      "name": "sala principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9467124931312298,
+          "pitch": -0.04132295495943339,
+          "rotation": 0,
+          "target": "1-dormitorio"
+        },
+        {
+          "yaw": -3.058110402893826,
+          "pitch": -0.2220430448563544,
+          "rotation": 0,
+          "target": "3-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bao",
+      "name": "baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118726167623942,
+          "pitch": 0.12851454986962807,
+          "rotation": 0,
+          "target": "2-sala-principal"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
